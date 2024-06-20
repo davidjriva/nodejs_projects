e962f2bb14820d1b5339843cb3e1495285f923cc
David Riva <davidjriva@gmail.com>
2024-06-20 12:34:06 -0700

    slugify url params

--- 1-node-farm/starter/README.md
+++ 1-node-farm/starter/README.md
@@ -2,5 +2,14 @@ This is a beginner project using Node.js, HTML, & CSS.
 
 It features a simulated online shopping platform for groceries, where a user will browse items and add/remove them from a shopping cart.
 
+Packages:
+    Slugify -- 
+    Nodemon -- Allows modification of the Node.js server while it's running
+
 Here is the command to run the server locally:
-    'npm run server'
+    'npm start'
+
+Future Actions:
+    - Add shopping cart to display added/removed items.
+    - Create dynamic price fluctuations to simulate market changes.
+    - Add more synthetic data and pages to the store-front.
--- 1-node-farm/starter/modules/render-pages.js
+++ 1-node-farm/starter/modules/render-pages.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { replaceTemplate } = require('./replace-template');
+const slugify = require('slugify');
 
 // Read data file synchronously (blocking call)
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
@@ -10,21 +10,30 @@ const cardTemplate = fs.readFileSync('./templates/card.html', 'utf-8');
 const overviewTemplate = fs.readFileSync('./templates/overview.html', 'utf-8');
 const productTemplate = fs.readFileSync('./templates/product.html', 'utf-8');
 
+// Generate slugs: (replace id in URL with the formatted product name)
+const slug_map = dataObj.reduce((acc, item) => {
+    acc[item.id] = slugify(item.productName, {lower: true, replacement: '-'});
+    return acc;
+}, {})
+
 function renderHomePage(res) {
     // Message contains HTML
     res.writeHead(200, { 'Content-Type': 'text/html' });
+
     // Save all card HTML to an array
     const cardsHTML = dataObj.map((item) => replaceTemplate(cardTemplate, item));
     
+    // Replace all cards placeholder with the HTML to render the cards
     const overviewHTML = overviewTemplate.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
     res.end(overviewHTML);
 }
 
 function renderProductPage(res, id) {
-    const product = dataObj.find(item => item.id == id);
-
-    if (product){
-        renderValidProduct(res, product);
+    const productName = id.replaceAll("-", " "); // Translate slug into original name of the product
+    const productObj = dataObj.find(item => item.productName.toLowerCase() === productName);
+    
+    if (productObj){
+        renderValidProduct(res, productObj);
     } else {
         // No product was found with the given ID
         renderErrorPage(res);
@@ -51,6 +60,22 @@ function renderErrorPage(res) {
         'my-own-header': 'hello-world'
     });
     res.end('<h1> Page not found! </h1>');
+}
+
+// Function to fill in the HTML templates
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName)
+                    .replace(/{%IMAGE%}/g, product.image)
+                    .replace(/{%QUANTITY%}/g, product.quantity)
+                    .replace(/{%PRICE%}/g, `$${product.price}`)
+                    .replace(/{%FROM%}/g, product.from)
+                    .replace(/{%NUTRIENTS%}/g, product.nutrients)
+                    .replace(/{%DESCRIPTION%}/g, product.description)
+                    .replace(/{%ID%}/g, slug_map[product.id]); // Use slug_map for cleaner rendering of product id's as the product name
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
 }
 
 module.exports = { renderHomePage, renderProductPage, renderTestPage, renderErrorPage }
