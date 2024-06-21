5bcd49019381d04c272070ff487e0dcf14f6c02f
David Riva <davidjriva@gmail.com>
2024-06-20 19:30:53 -0700

    wait for multiple promises

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -16,7 +16,7 @@ const readFilePromise = (fileName) => {
 // Custom function that asynchronously writes a file and returns a promise
 const writeFilePromise = (fileName, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(fileName, data, (err) => {
+    fs.writeFile(fileName, data + '\n', (err) => {
       if (err) reject(err);
       resolve('File written successfully.');
     });
@@ -42,21 +42,61 @@ const writeFilePromise = (fileName, data) => {
 //     console.log(err.message);
 //   });
 
-// Best Solution: Use the async/await features to further enhance code readability
+// Even Better Solution: Use the async/await features to further enhance code readability
 
 // Code in this function will automatically run asynchronously without blocking the event loop and will return a promise
+// const getDogPic = async () => {
+//   try {
+//     // Stops code from running until promise returns then stores the promise into the data variable
+//     const data = await readFilePromise(`${__dirname}/dog.txt`);
+//     console.log(`Breed: ${data}`);
+
+//     // Connect to API to get dog image
+//     const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+//     // Write link to dog image into a text file
+//     await writeFilePromise('dog-img.txt', res.body.message);
+//     console.log('Dog pic saved successfully!');
+//   } catch (err) {
+//     console.log(err);
+//     throw err; // Pass err up the chain
+//   }
+
+//   return '2: Async function has completed';
+// };
+
+// A special design pattern containing an unnamed function that's immediately called.
+// (async () => {
+//   try {
+//     console.log('1: Getting dog pics...');
+//     const msg = await getDogPic();
+//     console.log(msg);
+//     console.log('3: Done getting dog pics :D');
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// Even Better Solution: Use the async/await features to further enhance code readability & wait for multiple promises simultaneously!
 const getDogPic = async () => {
   try {
     // Stops code from running until promise returns then stores the promise into the data variable
     const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    // Connect to API to get dog image
-    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    // Connect to API to get dog images
+    const res1 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const res2 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const res3 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
-    // Write link to dog image into a text file
-    await writeFilePromise('dog-img.txt', res.body.message);
-    console.log('Dog pic saved successfully!');
+    // Wait for all three dog pic promises to resolve, then extract the image links
+    const allDogPicPromises = await Promise.all([res1, res2, res3]);
+    const images = allDogPicPromises.map((res) => res.body.message);
+    
+    // Write all three dog pics simultaneously
+    await writeFilePromise('dog-img.txt', images.join('\n'));
+
+    console.log('Dog pics saved successfully!');
   } catch (err) {
     console.log(err);
     throw err; // Pass err up the chain
@@ -65,7 +105,6 @@ const getDogPic = async () => {
   return '2: Async function has completed';
 };
 
-// A special design pattern containing an unnamed function that's immediately called.
 (async () => {
   try {
     console.log('1: Getting dog pics...');
