85eaefd06dd66b49b74f36facb6364736bce00a7
David Riva <davidjriva@gmail.com>
2024-06-23 20:57:45 -0700

    ingestion script

--- natours/starter/dev-data/data/import-dev-data.js
+++ natours/starter/dev-data/data/import-dev-data.js
@@ -1,5 +1,8 @@
 /*
     Script to ingest the user & tour data into MongoDB
+    Has two options:
+        --import: Ingests all data from the tour.json file into the MongoDB
+        --delete: Clears all data from MongoDB
 */
 
 const fs = require('fs');
@@ -21,3 +24,39 @@ mongoose
   .then(() => {
     console.log('DB connection successful');
   });
+
+// Read JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  } finally {
+    process.exit();
+  }
+};
+
+// Delete all data from the DB
+const deleteAllData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('All data has been deleted...');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    process.exit();
+  }
+};
+
+if (process.argv[2] == '--import') {
+    importData();
+} else if (process.argv[2] == '--delete'){
+    deleteAllData();
+} else {
+    console.log('Improper arguments passed. Please pass --import or --delete as options.')
+}
