ed28707c3030494e3aa59aad3bb69c6699f5ec1d
David Riva <davidjriva@gmail.com>
2024-06-28 12:14:48 -0700

    start of aggregation pipeline

--- natours/starter/controllers/tourController.js
+++ natours/starter/controllers/tourController.js
@@ -96,4 +96,38 @@ const deleteTour = async (req, res) => {
   }
 };
 
-module.exports = { aliasTopTours, getTours, getTour, createTour, updateTour, deleteTour };
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err,
+    });
+  }
+};
+
+module.exports = { aliasTopTours, getTours, getTour, createTour, updateTour, deleteTour, getTourStats };
--- natours/starter/routes/tourRoutes.js
+++ natours/starter/routes/tourRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
-const { aliasTopTours, getTours, getTour, createTour, updateTour, deleteTour } = require(`${__dirname}/../controllers/tourController`);
+const { aliasTopTours, getTours, getTour, createTour, updateTour, deleteTour, getTourStats } = require(`${__dirname}/../controllers/tourController`);
 
 const router = express.Router();
 
 // Parameter specific middleware; it only runs on requests with an id param and validates it
 router.route('/top-5-cheap')
     .get(aliasTopTours, getTours);
+
+router.route('/tour-stats')
+    .get(getTourStats);
 
 router.route('/')
     .get(getTours)
