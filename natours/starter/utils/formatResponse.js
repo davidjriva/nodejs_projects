9ecf57191ee8fb7a2c0be092b514baef0468e50f
David Riva <davidjriva@gmail.com>
2024-06-29 15:09:12 -0700

    advanced error handling

--- natours/starter/utils/formatResponse.js
+++ natours/starter/utils/formatResponse.js
@@ -9,10 +9,3 @@ exports.formatResponse = (res, statusCode, data) => {
 
   res.status(statusCode).json(response);
 };
-
-exports.formatError = (res, err) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-}
