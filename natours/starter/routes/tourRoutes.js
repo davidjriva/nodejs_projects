fc82d9e966142e0a0b4b39b9bf6dd984da864ec8
David Riva <davidjriva@gmail.com>
2024-07-26 13:42:22 -0700

    useful scripts

--- natours/starter/routes/tourRoutes.js
+++ natours/starter/routes/tourRoutes.js
@@ -18,10 +18,11 @@ router.route('/monthly-plan/:year')
 router.route('/')
     .get(authController.protect, tourController.getTours)
     .post(tourController.createTour);
-    
-router.route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
