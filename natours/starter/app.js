dcb8c169538e496cd120b53fc4c2d3878dc294cb
David Riva <davidjriva@gmail.com>
2024-07-27 16:55:44 -0700

    xss and noSQL defense

--- natours/starter/app.js
+++ natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const { StatusCodes } = require('http-status-codes');
 const AppError = require('./utils/appError');
@@ -33,6 +35,12 @@ app.use('/api', limiter);
 
 // Body parsers - reading data from the body into req.body
 app.use(express.json({ limit: '10kb' })); // Adds the body into the request object as JSON.
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
