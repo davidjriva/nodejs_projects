ba384625b581b9a439a4dd015f181171f1d8a1d7
David Riva <davidjriva@gmail.com>
2024-07-30 20:04:06 -0700

    protect modifying tour resources (opens up gets)

--- natours/starter/routes/tourRoutes.js
+++ natours/starter/routes/tourRoutes.js
@@ -19,13 +19,13 @@ router.route('/monthly-plan/:year')
     .get(tourController.getMonthlyPlan);
 
 router.route('/')
-    .get(authController.protect, tourController.getTours)
-    .post(tourController.createTour);
+    .get(tourController.getTours)
+    .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour)
   .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
