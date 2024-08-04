2b07753943c58be892bec9305f88778138e6574f
David Riva <davidjriva@gmail.com>
2024-08-04 16:51:01 -0700

    tour/slug route

--- nature-nomads/controllers/viewController.js
+++ nature-nomads/controllers/viewController.js
@@ -11,15 +11,25 @@ exports.getOverview = catchAsync(async (req, res) => {
   // 2.) Build template
 
   // 3.) Render the template using the tour data from step 1
-
   res.status(StatusCodes.OK).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(StatusCodes.OK).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1.) Get tour data, for the requested tour (including reviews and guides)
+  const slug = req.params.slug;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // 2.) Build template
+  console.log(tour);
+  // 3.) Render template using data from step 1
+  res.status(StatusCodes.OK).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
