ebca6aad1f78967f996f7b6f84fe4f3e9588e9b8
David Riva <davidjriva@gmail.com>
2024-08-06 18:22:08 -0700

    overwrite old files on upload

--- nature-nomads/controllers/userController.js
+++ nature-nomads/controllers/userController.js
@@ -33,7 +33,7 @@ exports.resizeUserPhoto = (req, res, next) => {
     return next();
   }
 
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.filename = `user-${req.user.id}.jpeg`;
 
   sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({ quality: 90 }).toFile(`public/img/users/${req.file.filename}`);
 
